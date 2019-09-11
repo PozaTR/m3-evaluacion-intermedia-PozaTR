015fa8b89ba0b898106168f395a202d80a0cfcee
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-09-11 08:24:45 +0200

    prepare the project to start work

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Pokemons from './components/Pokemons';
+
+class App extends React.Component {
+  render() {
+    return (
+  
+      );
+  }
+ 
+}
+
+export default App;
