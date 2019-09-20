80664371c4eb42548e0f553bba124f305aec6d47
Rocío Poza <roci@MacBook-Pro-de-carlos.local>
2019-09-20 21:15:02 +0200

    create docs folder

---
+++ docs/precache-manifest.1c6a5b89fc1075fd56c7c11df5e92a9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3386200fc073ad6d896126cb1afb8d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "432657853a5e74866832",
+    "url": "./static/css/main.efaadbfc.chunk.css"
+  },
+  {
+    "revision": "9623aaa4a92954310296",
+    "url": "./static/js/2.40aad0fb.chunk.js"
+  },
+  {
+    "revision": "432657853a5e74866832",
+    "url": "./static/js/main.76e9f81f.chunk.js"
+  },
+  {
+    "revision": "53d64cbcca118c2e152a",
+    "url": "./static/js/runtime~main.d64d0077.js"
+  }
+]);
